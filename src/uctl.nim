6d73e3bee6e2e0b16fbfb77f0278a4e9b1691bde
litlighilit <litlighilit@foxmail.com>
2025-05-28 17:45:46 +0800

    feat(cli): rel value e.g. +1%

--- src/uctl.nim
+++ src/uctl.nim
@@ -1,7 +1,8 @@
 
 import ./uctl/[util, help]
 import std/cmdline
-from std/strutils import parseFloat, endsWith, formatFloat, FloatFormatMode
+from std/strutils import parseFloat, startsWith, endsWith, formatFloat, FloatFormatMode
+from std/parseutils import parseFloat
 
 proc exitWithHelp() =
   priHelp()
@@ -16,6 +17,23 @@ proc chkErr(res: CmdExecRes) =
   of csSucc:
     discard
 
+proc query(subCmd: string): float =
+  let res = exec(subCmd)
+  res.chkErr
+  res.res
+
+proc parseMayPercent(sval: string): float =
+  if sval.endsWith '%':
+    let n = parseFloat(sval.toOpenArray(0, sval.len-2), result)
+    if n == 0:
+      #raise newException(ValueError,
+      quit(
+        "bad argument, expect a float (may suffixed with `%`), but got " & sval
+      )
+    result / 100
+  else:
+    parseFloat sval
+
 when isMainModule:
   let argn = paramCount()
   if argn == 0:
@@ -25,14 +43,11 @@ when isMainModule:
     exitWithHelp()
   if argn == 2:
     var sval = paramStr(2)
-    let val = if sval.endsWith '%':
-      sval.setLen sval.len - 1
-      sval.parseFloat / 100
-    else:
-      sval.parseFloat  
+    let rel = sval[0] in "+-"
+    var val = sval.parseMayPercent
+    if rel:
+      val += subCmd.query
     let res = exec(subCmd, val)
     res.chkErr
   else:  # argn == 1
-    let res = exec(subCmd)
-    res.chkErr
-    echo (res.res * 100).formatFloat(ffDecimal, 1) & "%"
+    echo (subCmd.query * 100).formatFloat(ffDecimal, 1) & "%"
